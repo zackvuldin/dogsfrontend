308823707d7278208b7eae007cda0771ea5f5c98
zackvuldin <y.zachary@gmail.com>
2020-10-20 09:57:50 -0700

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Form from './Form';
 
 function App() {
 	// URL VARIABLE
-	const url = 'http://localhost:4500';
+	const url = 'https://dogsbackend2.herokuapp.com';
 	// STATE TO HOLD DOGS
 	const [dogs, setDogs] = React.useState([]);
 	// Empty dog for Form
